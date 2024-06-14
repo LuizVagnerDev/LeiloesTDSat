e309654fdc1a55c8f3ae94a97e6648ab3eef0785
Luiz Vagner da Silva <luizvagnerdev@gmail.com>
2024-06-14 15:45:20 -0300

    Ajustado o código para a conexao com o BD na classe ProdutosDAO e deletada a classe conectaDAO, por não ser mais utilizada.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -10,32 +10,83 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
     
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    public Connection conexao;
+    public PreparedStatement stm;
+    public ResultSet rs;
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    private String url = "jdbc:mysql://127.0.0.1:3306/uc11?useSSL=false";
+    private String user = "root";
+    private String password = "Gigi@2010";
+   
+    
+    public boolean conectar(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection(url, user, password);
+            return true;
+        }
+        catch(ClassNotFoundException |SQLException ex){
+            
+            return false;
+            
+        }
+    }
+    
+    
+ 
+    
+    public int cadastrarProduto (ProdutosDTO produto){
         
-        
-        //conn = new conectaDAO().connectDB();
-        
+        try{
+            if(!produto.getNome().isEmpty() || !produto.getValor().toString().isEmpty()){
+                
+                 stm = conexao.prepareStatement("INSERT INTO produtos (id, nome, valor, status) VALUES (?,?, ?, ?)");
+                 stm.setInt(1, produto.getId());
+                 stm.setString(2, produto.getNome());
+                 stm.setInt(3, produto.getValor());
+                 stm.setString(4, produto.getStatus());
+            
+            
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Os campos não podem ficar em branco");    
+            }
+            
+           return stm.executeUpdate();
+            
+        }
+        catch(SQLException ex){
+            return ex.getErrorCode();
+        }
         
     }
     
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
     }
     
     
+    
+    public void desconectar(){
+        try{
+            conexao.close();
+        }
+        catch(SQLException ex){
+            System.out.println("Erro ao desconectar "+ ex.getMessage());
+            
+        }
+    }
     
         
 }
